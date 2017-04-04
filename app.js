1c38a8841368b623946417c209001ac7fe8137ea
Devstroyer <kudlicki.krzysztof@gmail.com>
2017-04-04 10:56:27 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passportConfig = require('./authentication/passportConfig');
+var auth = require('./authentication/middleware');
+var flash = require('express-flash');
+var helpers = require('./controllers/helpers');
+var logger = require('winston');
+var loggerConfig = require('./logging/logging-config');
+
+var index = require('./controllers/index');
+var users = require('./controllers/users');
+var login = require('./controllers/login');
+var logout = require('./controllers/logout');
+var tajemnice = require('./controllers/tajemnice');
+var registration = require('./controllers/registration');
+
+// // Tworzy tabelę użytkowników, jeśli jakaś istniała ją dropi
+// const User = require("./models/user");
+// User.User.sync({force: true});
+
+var app = express();
+loggerConfig.initialize();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan('combined', { stream: { write: loggerConfig.logHttpRequest }}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Logowanie i sesje
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session');
+app.use(session({
+  secret: 'straszliwy',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(passportConfig.serialize);
+passport.deserializeUser(passportConfig.deserialize);
+passport.use(new LocalStrategy(passportConfig.checkPassword));
+
+// Dodaje obiekt danych widoku wykorzystywany przez layout.pug i potencjalnie inne pugi
+app.use(function(req, res, next) {
+    req.viewData = helpers.createCommonViewData(req);
+    next();
+});
+app.use('/', index);
+app.use('/users', users);
+app.use('/login', login);
+app.use('/logout', logout);
+app.use('/registration', registration);
+
+// W ten sposób można ustalić ścieżkę, dla której zawsze wymagane bedzie logowanie
+// Np. app.use('/student', auth.authenticate());, i potem wszystkie adresy typu 
+// /student/podania itd. będą wymagały logowania
+app.use('/tajemnice', auth.authenticate());
+app.use('/tajemnice', tajemnice);
+
+// Strony tylko dla administratorów
+app.use('/admin', auth.authenticate());
+app.use('/admin', auth.authorize('1'));
+
+// Strony tylko dla nauczycieli
+app.use('/teacher', auth.authenticate());
+app.use('/teacher', auth.authorize('2'));
+
+// Strony tylko dla studentów
+app.use('/student', auth.authenticate());
+app.use('/student', auth.authorize('3'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  logger.debug('Strona nie istnieje: ' + req.url);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  logger.error(err);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
