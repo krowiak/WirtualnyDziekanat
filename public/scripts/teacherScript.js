fc177153e17129d02a562f7812c452e0cd0d40e6
Devstroyer <kudlicki.krzysztof@gmail.com>
2017-06-03 15:23:32 +0200

    jeszcze niedzialajace przypisywanie ocen

--- public/scripts/teacherScript.js
+++ public/scripts/teacherScript.js
@@ -28,8 +28,33 @@ function showSubject(index){
 			$('#asignedStudentsTable tbody').append( '<tr><td>'+ Subjects[index].users[i].id +'</td>' +'<td>' 
 											    + Subjects[index].users[i].firstName+'</td>'+'<td>' 
 											    + Subjects[index].users[i].lastName+'</td>'+'<td>'
-											    +'</td></tr>');	
+											    +'<input id="fstAttempt" type="number" name="quantity" min="0" max="5" step="0.5" class="form-control" placeholder="'+Subjects[index].users[i].grades[1].grade+'">'+'</td><td>'
+											    +'<input id="scndAttempt"type="number" name="quantity" min="0" max="5" step="0.5" class="form-control" placeholder="">'+'</td><td>'
+											    + '<button onClick="saveGrades('+i+')" type="button" class="btn btn-success">Save'+
+											    '</button></td></tr>' );	
 	}
+}
+
+function showMessage(msg){
+	$('.alert').first().remove();
+	$('.container').first().prepend('<div class="alert alert-info">'+msg+'</div>');
+}
+
+
+//funkcja na pałe na razie do poprawy!!
+function saveGrades(index){
+	jQuery.ajax({
+	    url: "subjects/grade",
+	    type:"POST",
+	    dataType: "json",
+	    data: { userId: Subjects[0].users[index].id, subjectId: 1,grade: parseFloat($("#fstAttempt").val()).toFixed(2), attempt: 2},
+	    success: function(data) {
+	    	showMessage(data.message);     
+	    },
+	    error: function() {
+	        //Do alert is error
+	    }
+	});
 }
 
 function getSubjectInfo(){
@@ -39,6 +64,7 @@ function getSubjectInfo(){
 	    dataType: "json",
 	    data: { where: {name: {like: '%'+text+'%'}}},
 	    success: function(data) {
+	    	alert(JSON.stringify(data))
 	    	Subjects=data;  
 	    	createTable(data);      
 	    },
